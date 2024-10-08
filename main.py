8a5117d9b2a320f31d12d31a3b1d70840149105a
unknown <dashulja861@mail.com>
2024-10-08 16:15:03 +0100

    add output and input

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import speech_recognition as sr
-
 from output import say, engine
+import json
+import random
 
 
 
+def load_speech():
+    with open("speech.json", 'r') as file:
+        data = json.load(file)
+    return data
 
-# for index, name in enumerate(sr.Microphone.list_microphone_names()):
-#     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+data = load_speech()
 
-
+def speech_commands(text:str):
+    for phrase in data:
+        for input_words in phrase['input']:
+            if input_words in text.lower():
+                output = random.choice(phrase['output'])
+                say(output)
+                print(output)
+            
 
 # obtain audio from the microphone
 def main():
@@ -17,7 +28,7 @@ def main():
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
         
-        say("Hello, I am your voice assistant. How can I help you?")
+        # say("Hello, I am your voice assistant. How can I help you?")
         
         while True:
             engine.runAndWait()
@@ -27,10 +38,8 @@ def main():
             #try-except errors
             try:
                 text = recognizer.recognize_google(audio, language='en-GB')
-                if text.lower() == "hello":
-                    say("Hi, how are you doing")
-                    
                 print(f"You said: {text}")
+                speech_commands(text)
             except sr.UnknownValueError:
                 print("Audio is not recognized")   
             except sr.RequestError:
